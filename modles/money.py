1b71ae31f62a97eecd2181a34c666b9e4ae6405d
Haozhe Chen <chz_chen@outlook.com>
2019-10-23 14:27:48 +1100

    Finished initate Money class

--- modles/money.py
+++ modles/money.py
@@ -1,7 +1,28 @@
 import pandas
 
-data = pandas.read_html("https://rate.bot.com.tw/xrt?Lang=zh-TW")[0]
-currency_table = data.ix[:,0:5]
-currency_table.columns = ["Currency","Currency-Buy","Currency-Sell","RealTime-Buy", "RealTime-Sell",]
-currency_table["Currency"] = currency_table["Currency"].str.extract("\((\w+)\)",expand=True)
-print(currency_table)
+class Money(object):
+    def __init__(self, currency, cash_in, cash_out, sign_in, sign_out):
+        self.currency = currency
+        self.cash_in = cash_in
+        self.cash_out = cash_out
+        self.sign_in = sign_in
+        self.sign_out = sign_out
+
+    @staticmethod
+    def search_data():
+        data = pandas.read_html("https://rate.bot.com.tw/xrt?Lang=zh-TW")[0]
+        currency_table = data.ix[:,0:5]
+        currency_table.columns = ["Currency","Cash_in","Cash_out","Sign_in", "Sign_out",]
+        currency_table["Currency"] = currency_table["Currency"].str.extract("\((\w+)\)",expand=True)
+        #print(currency_table)
+
+        moneydict = {}
+        position = {}
+        for index in range(0, 19):
+            dollar = currency_table.values[index]
+            moneydict[index] = Money(dollar[0], dollar[1], dollar[2], dollar[3], dollar[4])
+            position[dollar[0]] = index
+        return moneydict, position
+
+# moneydict, position = Money.search_data()
+# print(position)
