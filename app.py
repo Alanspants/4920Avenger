24181c957145e50fd1cc57d08d1a881575d67a9a
Haozhe Chen <chz_chen@outlook.com>
2019-10-26 11:48:03 +1100

    Set the homepage and currency table

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+from modles.money import Money
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("base.html")
+    moneydict, position = Money.search_data()
+    return render_template("home.html", moneydict = moneydict)
 
 if __name__ == "__main__":
     app.run(debug=True, port=4100)
- 
