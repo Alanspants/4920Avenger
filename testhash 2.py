8d319675516a5dd1cbdf0849242c1c0e20e6ffb4
Haozhe Chen <chz_chen@outlook.com>
2019-10-28 15:16:36 +1100

    clear git repository

--- testhash 2.py
+++
@@ -1,7 +0,0 @@
-from passlib.hash import pbkdf2_sha512
-
-password = input("password")
-hash_password = pbkdf2_sha512.encrypt(password)
-print(hash_password)
-print(pbkdf2_sha512.verify(password,hash_password))
-
